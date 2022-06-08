f160db6c2ccd80865cb65ff8fc601c5e186e716e
playboi116 <playboi116@ya.ru>
2022-06-08 23:48:41 +0300

    temp c

---
+++ temp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+  int z = 1/10;
+  float x = 1.0/10.0;
+  double y = 1.0/10.0;
+  
+  printf("int:\n%d\n\n", z);
+  printf("float:\n4\t--\t%.4f\n10\t--\t%.12f\n16\t--\t%.16f\n\n", x,x,x);
+  printf("double:\n4\t--\t%.4f\n10\t--\t%.12f\n16\t--\t%.16f\n", y,y,y);
+  
+  return(0);
+}
